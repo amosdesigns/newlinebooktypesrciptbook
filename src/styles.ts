8fadde8d745e8c9df948da3d8c89e428ce99e8db
Jerome Amos <mail@amosdesigns.net>
2020-05-25 19:40:02 -0400

    style correction

--- src/styles.ts
+++ src/styles.ts
@@ -15,7 +15,10 @@ export const ColumnContainer = styled.div`
 background-color: #ebecf0;
 width: 300px;
 min-height: 40px;
-margin-right: 20px; border-radius: 3px; padding: 8px 8px; flex-grow: 0;
+margin: 20px; 
+border-radius: 3px; 
+padding: 8px 8px; 
+flex-grow: 0;
 `;
 
 export const ColumnTitle = styled.div`
@@ -27,13 +30,13 @@ export const CardContainer = styled.div`
 background-color:#fff;
 cursor: pointer;
 margin-bottom: 0.5rem;
+margin-right: 0.5rem;
 padding: 0.5rem 1rem;
 max-width: 300px;
 border-radius: 3px;
 font-weight: bold;
 box-shadow: #091e4242 0px 1px 0px 0px;
 `;
-
 interface AddItemButtonProps {
     dark?: boolean;
 }
